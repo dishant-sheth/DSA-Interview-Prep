b5c61e2534bbb5d69dcf2a2f4216a5e3a8652ba0
Dishant Sheth <dishantnirov.sheth2015@vit.ac.in>
2019-07-05 02:03:36 +0530

    Fixes

--- Cracking the Coding Interview - GLM/1. Arrays and Strings/ZeroMatrix.java
+++ Cracking the Coding Interview - GLM/1. Arrays and Strings/ZeroMatrix.java
@@ -5,8 +5,8 @@ public class ZeroMatrix{
     /*
         APPROACH 1 -> Use a HashSet to store the row & col indexes where the matrix elememt is zero.
     */
-    public void solve1(int[][] matrix){
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+    public int[][] solve1(int[][] matrix){
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return matrix;
         HashSet<Integer> rows = new HashSet<>();
         HashSet<Integer> cols = new HashSet<>();
         int n = matrix.length; //Number of rows in input matrix
@@ -35,26 +35,29 @@ public class ZeroMatrix{
             }
         }
 
+        return matrix;
+
     }
 
     /*
-        [DOESN'T WORK YET.]
         APPROACH 2 -> Use the first row and first col as flag for the rest of the matrix.
     */
-    public void solve2(int[][] matrix){
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+    public int[][] solve2(int[][] matrix){
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return matrix;
         int n = matrix.length; //Number of rows in input matrix
         int m = matrix[0].length; //Number of cols in input matrix
         //Since, the element at (0,0) is common for both first row and first col, we'll use it for the first row and use a boolean flag for the first col.
         boolean firstCol = false;
-        //Check first col for zeros
+        
+        //1. Check COL-0 for zeroes
         for(int i=0; i<n; i++){
             if(matrix[i][0] == 0){
                 firstCol = true;
                 break;
             }
         }
-        //Check first row for zeros
+
+        //2. Check ROW-0 for zeroes
         for(int j=0; j<m; j++){
             if(matrix[0][j] == 0){
                 matrix[0][0] = 0;
@@ -62,7 +65,7 @@ public class ZeroMatrix{
             }
         }
 
-        //Check the remaining matrix
+        //3. Check remaining matrix for zeroes
         for(int i=1; i<n; i++){
             for(int j=1; j<m; j++){
                 if(matrix[i][j] == 0){
@@ -72,7 +75,7 @@ public class ZeroMatrix{
             }
         }
 
-        //Mark rows and cols as zeros
+        //4. Set rows as zeroes. (except ROW-0)
         for(int i=1; i<n; i++){
             if(matrix[i][0] == 0){
                 for(int j=1; j<m; j++){
@@ -81,27 +84,35 @@ public class ZeroMatrix{
             }
         }
 
+        //5. Set cols as zeroes. (except COL-0)
         for(int j=1; j<m; j++){
             if(matrix[0][j] == 0){
                 for(int i=1; i<n; i++){
                     matrix[i][j] = 0;
                 }
             }
-        }       
-        
+        }
+
+        //6. Check for COL-0
         if(firstCol){
             for(int i=0; i<n; i++){
                 matrix[i][0] = 0;
             }
         }
 
+        //7. Check for ROW-0
+        if(matrix[0][0] == 0){
+            for(int j=0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        return matrix;
 
     }
 
-    public static void main(String[] args) {
-        ZeroMatrix zeroMatrix = new ZeroMatrix();
-        int[][] matrix = {{1,2,3,4}, {5,0,0,8}, {9,10,11,12}};
-        zeroMatrix.solve1(matrix);
+    //Helper to print the matrix
+    private void printMatrix(int[][] matrix){
         int n = matrix.length; //Number of rows in input matrix
         int m = matrix[0].length; //Number of cols in input matrix
         for(int i=0; i<n; i++){
@@ -111,12 +122,13 @@ public class ZeroMatrix{
             System.out.println();
         }
         System.out.println();
-        zeroMatrix.solve2(matrix);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                System.out.print(matrix[i][j] + "\t");
-            }
-            System.out.println();
-        }
+    }
+
+    public static void main(String[] args) {
+        ZeroMatrix zeroMatrix = new ZeroMatrix();
+        int[][] matrix = {{1,2,3,4}, {5,0,0,8}, {9,10,11,12}};
+        //Trying the better approach
+        matrix = zeroMatrix.solve2(matrix);
+        zeroMatrix.printMatrix(matrix);
     }
 }
